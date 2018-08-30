5db610a0f810f667d887463b93ff3d5aad6c8d28
prealge <34986064+prealge@users.noreply.github.com>
2018-08-30 13:41:49 -0400

    Update db.js

--- js/db.js
+++ js/db.js
@@ -9,5 +9,10 @@ var config = {
 };
 firebase.initializeApp(config);
 
+const firestore = firebase.firestore();
+const settings = {/* your settings... */ timestampsInSnapshots: true};
+firestore.settings(settings);
+
+
 // Initialize Cloud Firestore through Firebase
 var db = firebase.firestore();
