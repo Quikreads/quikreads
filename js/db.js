fdf6ef06e4ee2bf40425dd704a2328f6ef14f8ac
prealge <34986064+prealge@users.noreply.github.com>
2018-08-30 13:10:08 -0400

    Update db.js

--- js/db.js
+++ js/db.js
@@ -9,15 +9,5 @@ var config = {
 };
 firebase.initializeApp(config);
 
-const firebase = require("firebase");
-// Required for side-effects
-require("firebase/firestore");
-
-firebase.initializeApp({
-  apiKey: '### FIREBASE API KEY ###',
-  authDomain: '### FIREBASE AUTH DOMAIN ###',
-  projectId: '### CLOUD FIRESTORE PROJECT ID ###'
-});
-
 // Initialize Cloud Firestore through Firebase
 var db = firebase.firestore();
