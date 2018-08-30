3d896e302bceda2036a6e1cba26478f5fa7efd30
prealge <34986064+prealge@users.noreply.github.com>
2018-08-30 11:22:40 -0400

    Update db.js

--- js/db.js
+++ js/db.js
@@ -1,11 +1,23 @@
 // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDmlCjU652S7zTNGSM_SVI7N-9BC_binbs",
-    authDomain: "quikreads-f6766.firebaseapp.com",
-    databaseURL: "https://quikreads-f6766.firebaseio.com",
-    projectId: "quikreads-f6766",
-    storageBucket: "quikreads-f6766.appspot.com",
-    messagingSenderId: "308229798991"
-  };
-  firebase.initializeApp(config);
+var config = {
+  apiKey: "AIzaSyDmlCjU652S7zTNGSM_SVI7N-9BC_binbs",
+  authDomain: "quikreads-f6766.firebaseapp.com",
+  databaseURL: "https://quikreads-f6766.firebaseio.com",
+  projectId: "quikreads-f6766",
+  storageBucket: "quikreads-f6766.appspot.com",
+  messagingSenderId: "308229798991"
+};
+firebase.initializeApp(config);
 
+const firebase = require("firebase");
+// Required for side-effects
+require("firebase/firestore");
+
+firebase.initializeApp({
+  apiKey: '### FIREBASE API KEY ###',
+  authDomain: '### FIREBASE AUTH DOMAIN ###',
+  projectId: '### CLOUD FIRESTORE PROJECT ID ###'
+});
+
+// Initialize Cloud Firestore through Firebase
+var db = firebase.firestore();
